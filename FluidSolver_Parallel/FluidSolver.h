812b1471d41312c89cbc963f9a3c9dfb9698b711
SaumitraVinayJoshi <saumitra.vjoshi@gmail.com>
2016-02-09 19:39:37 +0100

    removed tabs

--- FluidSolver_Parallel/FluidSolver.h
+++ FluidSolver_Parallel/FluidSolver.h
@@ -10,32 +10,32 @@
 #include <mpi.h>
 
 void fluidInit(
-		int rank,
-		int chunkLength,
-		double *data
-		);
+    int rank,
+    int chunkLength,
+    double *data
+    );
 
 void fluidComputeSolution(
-		int rank,
-		int size,
-		int domainSize,
-		int chunkLength,
-		double kappa,
-		double tau,
-		double gamma,
-		double t,
-		double *pressure,
-		double *pressure_n,
-		double *pressure_old,
-		double *crossSectionLength,
-		double *crossSectionLength_n,
-		double *velocity,
-		double *velocity_n
-		);
+    int rank,
+    int size,
+    int domainSize,
+    int chunkLength,
+    double kappa,
+    double tau,
+    double gamma,
+    double t,
+    double *pressure,
+    double *pressure_n,
+    double *pressure_old,
+    double *crossSectionLength,
+    double *crossSectionLength_n,
+    double *velocity,
+    double *velocity_n
+    );
 
 void fluidDataDisplay(
-		double *data,
-		int counterLength
-		);
+    double *data,
+    int counterLength
+    );
 
 #endif
--- FluidSolver_Serial/fluid_nl.h
+++ FluidSolver_Serial/fluid_nl.h
@@ -3,24 +3,24 @@
 
 #define PI 3.14159265359
 
-	int fluid_nl ( double *crossSectionLength, 
-			 double *crossSectionLength_n, 
-		   	 double *velocity, 
-		   	 double *velocity_n, 
-		   	 double *pressure, 
-		   	 double *pressure_n, 
-		   	 double *pressure_old, 
-		   	 int	t, 
-		   	 int	N, 
-		   	 double kappa, 
-		   	 double tau, 
-		   	 double gamma
-		      );
+int fluid_nl ( double *crossSectionLength, 
+    double *crossSectionLength_n, 
+    double *velocity, 
+    double *velocity_n, 
+    double *pressure, 
+    double *pressure_n, 
+    double *pressure_old, 
+    int  t, 
+    int  N, 
+    double kappa, 
+    double tau, 
+    double gamma
+    );
 
-	int linsolve ( int n, 
-			 double **A, 
-			 double *b, 
-			 double *x 
-			);
+int linsolve ( int n, 
+    double **A, 
+    double *b, 
+    double *x 
+    );
 
 #endif
