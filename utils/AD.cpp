301fa6d836b12fc5e19bb47e5bccfbb0d47dcf72
Klaudius Scheufele <scheufks@ipvs.uni-stuttgart.de>
2016-09-09 13:36:59 +0200

    added AD data type to compute derivative in multiple directions with one function evaluation

--- utils/AD.cpp
+++ utils/AD.cpp
@@ -2,10 +2,11 @@
 
 #include <cmath>
 #include <assert.h>
-#include <boost/concept_check.hpp>
 
 namespace AD {
 
+  // dualReal methods ------------------------------------------
+  
   dualReal operator+(const dualReal& lhs, const dualReal& rhs){
    return  dualReal(lhs.u + rhs.u, lhs.v + rhs.v);
   }
@@ -23,9 +24,8 @@ namespace AD {
   }
   
   dualReal operator/(const dualReal& lhs, const dualReal& rhs){
-   if(lhs.u == 0) return dualReal(0.); 
-   if(rhs.u == 0)
-     return dualReal(lhs.u / rhs.u, 0);
+   if(lhs.u == 0 || rhs.u == 0) 
+     return dualReal(0.); 
    else
      return  dualReal(lhs.u / rhs.u, (lhs.v * rhs.u - lhs.u * rhs.v)/(rhs.u * rhs.u));
   }
@@ -60,5 +60,128 @@ namespace AD {
   dualReal sqrt(const dualReal& x){
    return dualReal(std::sqrt(x.u), .5*1./std::sqrt(x.u)*x.v); 
   }
+  
+  // dReal_grad methods ------------------------------------------
+  
+  dReal_grad operator+(const dReal_grad& lhs, const dReal_grad& rhs){
+    std::size_t n = std::max(lhs.v.size(), rhs.v.size());
+    std::size_t m = std::min(lhs.v.size(), rhs.v.size());
+    if (n == 1) 
+      return  dReal_grad(lhs.u + rhs.u, lhs.v[0] + rhs.v[0]);
+    std::vector<double> _v(n);
+    for(std::size_t i = 0; i < n; i++){
+      if(i < m)
+        _v[i] = lhs.v[i] + rhs.v[i];
+      else if(n == lhs.v.size())
+	_v[i] = lhs.v[i]; 
+      else
+	_v[i] = rhs.v[i];
+    }
+    return  dReal_grad(lhs.u + rhs.u, _v);
+  }
+  
+  dReal_grad operator-(const dReal_grad& lhs, const dReal_grad& rhs){
+   std::size_t n = std::max(lhs.v.size(), rhs.v.size());
+    std::size_t m = std::min(lhs.v.size(), rhs.v.size());
+    if (n == 1) 
+      return  dReal_grad(lhs.u - rhs.u, lhs.v[0] - rhs.v[0]);
+    std::vector<double> _v(n);
+    for(std::size_t i = 0; i < n; i++){
+      if(i < m)
+        _v[i] = lhs.v[i] - rhs.v[i];
+      else if(n == lhs.v.size())
+	_v[i] = lhs.v[i]; 
+      else
+	_v[i] = -rhs.v[i];
+    }
+    return  dReal_grad(lhs.u - rhs.u, _v);
+  }
+  
+  void operator-(dReal_grad& rhs){
+    for(std::size_t i = 0; i < rhs.v.size(); i++)
+      rhs.v[i] = -rhs.v[i];
+    rhs.u = -rhs.u;
+  }
+  
+  dReal_grad operator*(const dReal_grad& lhs, const dReal_grad& rhs){
+    std::size_t n = std::max(lhs.v.size(), rhs.v.size());
+    std::size_t m = std::min(lhs.v.size(), rhs.v.size());
+    if (n == 1) 
+      return  dReal_grad(lhs.u * rhs.u, lhs.v[0] * rhs.u + lhs.u * rhs.v[0]);
+    std::vector<double> _v(n);
+    for(std::size_t i = 0; i < n; i++){
+      if(i < m)
+        _v[i] = lhs.v[i] * rhs.u + lhs.u * rhs.v[i];
+      else if(n == lhs.v.size())
+	_v[i] = lhs.v[i] * rhs.u; 
+      else
+	_v[i] = rhs.v[i] * lhs.u;
+    }
+    return  dReal_grad(lhs.u * rhs.u, _v);
+  }
+  
+  dReal_grad operator/(const dReal_grad& lhs, const dReal_grad& rhs){
+    std::size_t n = std::max(lhs.v.size(), rhs.v.size());
+    std::size_t m = std::min(lhs.v.size(), rhs.v.size());
+    if(lhs.u == 0 || rhs.u == 0){
+        return dReal_grad(0.); 
+    }else if (n == 1){
+        return  dReal_grad(lhs.u / rhs.u, (lhs.v[0] * rhs.u - lhs.u * rhs.v[0])/(rhs.u * rhs.u));
+    }else{
+      std::vector<double> _v(n);
+      for(std::size_t i = 0; i < n; i++){
+        if(i < m)
+          _v[i] = (lhs.v[i] * rhs.u - lhs.u * rhs.v[i])/(rhs.u * rhs.u);
+        else if(n == lhs.v.size())
+	  _v[i] = (lhs.v[i] * rhs.u)/(rhs.u * rhs.u); 
+        else
+	  _v[i] = (-lhs.u * rhs.v[i])/(rhs.u * rhs.u);
+      }
+      return  dReal_grad(lhs.u / rhs.u, _v);
+    }
+  }
+  
+  void sin(dReal_grad& x){
+    for(std::size_t i = 0; i < x.v.size(); i++)
+      x.v[i] = x.v[i]*std::cos(x.u);
+    x.u = std::sin(x.u);
+  }
+  
+  void cos(dReal_grad& x){
+    for(std::size_t i = 0; i < x.v.size(); i++)
+      x.v[i] = -x.v[i]*std::sin(x.u);
+    x.u = std::cos(x.u);
+  }
+  
+  void exp(dReal_grad& x){
+    for(std::size_t i = 0; i < x.v.size(); i++)
+      x.v[i] = x.v[i]*std::exp(x.u);
+    x.u = std::exp(x.u);
+  }
+  
+  void log(dReal_grad& x){
+    if(x.u != 0)
+      for(std::size_t i = 0; i < x.v.size(); i++)
+        x.v[i] = x.v[i]/x.u;
+    x.u = std::log(x.u);
+  }
+  
+  void pow(dReal_grad& x, int e){
+    for(std::size_t i = 0; i < x.v.size(); i++)
+      x.v[i] = e*std::pow(x.u,e-1)*x.v[i];
+    x.u = std::pow(x.u, e);
+  }
+  
+  void abs(dReal_grad& x){
+    for(std::size_t i = 0; i < x.v.size(); i++)
+      x.v[i] = x.v[i]*(x.u/std::abs(x.u));
+    x.u = std::abs(x.u);
+  }
+  
+  void sqrt(dReal_grad& x){
+    for(std::size_t i = 0; i < x.v.size(); i++)
+      x.v[i] = .5*1./std::sqrt(x.u)*x.v[i];
+    x.u = std::sqrt(x.u);
+  } 
   
 }
--- utils/AD.hpp
+++ utils/AD.hpp
@@ -1,6 +1,8 @@
 #ifndef AD_H_
 #define AD_H_
 
+#include <vector>
+
 namespace AD {
 
   struct dualReal{
@@ -11,6 +13,48 @@ namespace AD {
     dualReal(double _u) : u(_u), v(0) {}
     dualReal(double _u, double _v) : u(_u), v(_v) {}
   };
+  
+  
+  // allows to take the derivative in all (or multiple) directions with a single function evaluation.
+  struct dReal_grad{
+    
+    double u;
+    std::vector<double> v;
+    dReal_grad()                                   : u(0),  v()  {v.resize(1); v[0] = 0.;}
+    dReal_grad(int n)                              : u(0),  v()  {v.resize(n); for (int i=0; i < n; i++){ v[i] = 0.;}}
+    dReal_grad(double _u)                          : u(_u), v()  {v.resize(1); v[0] = 0.;}
+    dReal_grad(double _u, int n)                   : u(_u), v()  {v.resize(n); for (int i=0; i < n; i++){ v[i] = 0.;}}
+    dReal_grad(double _u, double _v)               : u(_u), v(1) {v[0] = _v;}
+    dReal_grad(double _u, std::vector<double>& _v) : u(_u), v()  {v.resize(_v.size()); for (std::size_t i=0; i < _v.size(); i++){ v[i] = _v[i];}}
+  };
+  
+  // dReal_grad methods
+  
+  dReal_grad operator+(const dReal_grad& lhs, const dReal_grad& rhs);
+  
+  dReal_grad operator-(const dReal_grad& lhs, const dReal_grad& rhs);
+  
+  void operator-(dReal_grad& rhs);
+  
+  dReal_grad operator*(const dReal_grad& lhs, const dReal_grad& rhs);
+  
+  dReal_grad operator/(const dReal_grad& lhs, const dReal_grad& rhs);
+  
+  void sin(dReal_grad& x);
+  
+  void cos(dReal_grad& x);
+  
+  void exp(dReal_grad& x);
+  
+  void log(dReal_grad& x);
+  
+  void pow(dReal_grad& x, int e);
+  
+  void abs(dReal_grad& x);
+  
+  void sqrt(dReal_grad& x);
+  
+  // dualReal methods
   
   dualReal operator+(const dualReal& lhs, const dualReal& rhs);
   
