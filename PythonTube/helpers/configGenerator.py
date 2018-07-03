058bab2bb39e59edbf5dfd2448b44912f23f52a1
Benjamin Rüth <benjamin.rueth@tum.de>
2018-07-03 10:21:12 +0200

    Adding some explanations

--- PythonTube/helpers/configGenerator.py
+++ PythonTube/helpers/configGenerator.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import configuration_file as config
 
-template = "helpers/precice-config.xml"
+template = "helpers/precice-config.xml"  # template precice-config.xml used to generate series of configs
 basepath = os.path.dirname(template)
 basename = os.path.basename(template)
 
@@ -15,23 +15,23 @@ assert np.abs(float(tree.findall('**/max-time')[0].attrib['value']) - config.T_m
 
 ## vary timestep length and generate multiple precice-config.xml from template
 
-varied_variable = "timestep-length"
+varied_variable = "timestep-length"  # corresponding tag in precice-config.xml template
+elem = tree.findall('**/'+varied_variable)[0]  # find tag in template
 
-tau0 = config.tau0
+## create list holding the values that you want to use for generating series of configs
+tau0 = config.tau0  # get smallest timestep length from python config
 N_experiments = config.n_tau
-varied_values = tau0 * .5**np.arange(0,N_experiments)
+varied_values = tau0 * .5**np.arange(0,N_experiments)  # create list of considered timestep sizes
 
-elem = tree.findall('**/'+varied_variable)[0]
-
+## create folder to write output to 
 outfolder = 'configs'
-
 if os.path.exists(outfolder):
     for file in os.listdir(outfolder):
         os.remove(os.path.join(outfolder,file))
     os.rmdir(outfolder)
-
 os.mkdir(outfolder)
 
+## iterate through list varied_values and create a precice-config.xml based on the given template with modified varied_variable
 for value in varied_values:
     elem.attrib['value'] = str(value)
     newname = os.path.splitext(basename)[0]+"_"+str(value).replace('.','_')+".xml"
