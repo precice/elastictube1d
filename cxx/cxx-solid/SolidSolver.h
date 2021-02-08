b46a9606a4d3205b3f324564c49c5a55e76d53ca
homspons <ge35joj>
2021-02-08 22:02:57 +0100

    delete solidDataDisplay

--- cxx/cxx-solid/SolidSolver.h
+++ cxx/cxx-solid/SolidSolver.h
@@ -1,7 +1,5 @@
 #pragma once
 
-void SolidInit(int chunkLength, double* data);
 
 void SolidComputeSolution(int rank, int size, int chunkLength, double* pressure, double* crossSectionLength);
 
-void SolidDataDisplay(double* data, int length);
