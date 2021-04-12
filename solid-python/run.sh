c9f79f8db4a6ce369bd1f2edee62bf2d145bf078
Gerasimos Chourdakis <makishourdakis@gmail.com>
2021-04-12 11:46:10 +0200

    Update run scripts for Python

--- solid-python/run.sh
+++ solid-python/run.sh
@@ -1,12 +1,4 @@
-#!/bin/bash
-cd ${0%/*} || exit 1    		    		# Run from this directory
+#!/bin/sh
+set -e -u
 
-# Solid participant
-
-# Run this script in one terminal in order to start this participant.
-
-echo "Preparing and running the Solid participant..."
-
-python3 ./SolidSolver.py ../precice-config.xml 
-
-echo ""
+python3 ./SolidSolver.py ../precice-config.xml
